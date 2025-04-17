99fe43086aa70d68f95427534dce74ece69e76b2
sofia <sofiaaristizabal23@gmail.com>
2025-04-17 12:26:43 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize:true,
+      ssl:{
+        rejectUnauthorized:false
+      }
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
